3b1c99bf356e8fad255b1fa4bf04b3b9008c3b4f
Andreas Heimann <andreas@iklikla.de>
2019-05-28 09:58:12 +0200

    fixed static js

--- src/static/js/pageLoader.js
+++ src/static/js/pageLoader.js
@@ -3,13 +3,3 @@ let domBody = document.querySelector("body");
 window.onload = function() {
     domBody.classList.remove("loading");
 };
-
-// Scrolling
-function smoothScrollTo(elementSelector) {
-    let elementToScroll = document.querySelector(elementSelector);
-
-    window.scroll({
-        top: elementToScroll.getBoundingClientRect().top,
-        behavior: 'smooth'
-    });
-}
